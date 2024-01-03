4d777171fc74b309550c03745912023f4dc50a61
Wesley Francescon <wfrancescons@gmail.com>
2024-01-02 22:14:17 -0300

    add correct 'start' and 'help' messages

--- src/commands/index.js
+++ src/commands/index.js
@@ -12,8 +12,8 @@ async function start(ctx) {
 
         await ctx.reply(
             `Hello, ${first_name} 👋\n` +
-            `\nWelcome to the telelast bot 🤖🎵\n` +
-            `\nUse /reg to set your Lastfm’s username\n` +
+            `\nWelcome to the letterboxd bot 🤖🎵\n` +
+            `\nUse /reg_lb to set your Letterboxd's username\n` +
             `\nType / or /help to see a list of valid commands\n` +
             `\nAccess @telelastfmnews for server status and news 📰`
         )
@@ -30,15 +30,8 @@ async function help(ctx) {
 
         await ctx.replyWithMarkdown(
             `Valid commands: 🤖\n` +
-            `\n/ln - Track you're scrobbling` +
-            `\n/alb - Album you're scrobbling` +
-            `\n/art - Artist you're scrobbling` +
-            `\n\`/reg lastfmuser\` - Set your LastFm's username` +
-            `\n\`/addn artist-nick\` - Set artist's nick for a chat` +
-            `\n\`/rmvn artist\` - Remove artist's nick for a chat` +
-            `\n\`/collage columnsxrows period\` - Generate a grid collage` +
-            `\n\`/top mediatype period\` - Generate a top scrobbles collage` +
-            `\n\`/story mediatype period\` - Generate a image of your latest scrobble`
+            `\n/lb - See your last watched movie` +
+            `\n\`/reg_lb letterboxduser\` - Set your Letterboxd's username`
         )
 
     } catch (error) {
@@ -50,4 +43,3 @@ export {
     help, lb, reg,
     start
 }
-
